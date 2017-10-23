7538a296fd7cdd4632723225a1fd1f07621d4b26
Jorge Crespo <jcrespo@jcrespoPC>
2017-10-23 11:50:42 -0300

    Se empieza a añadir el testeo en jenkins

---
+++ test/bema_test.c
@@ -0,0 +1,24 @@
+/*
+ * bema_test.c
+ *
+ *  Created on: 23/10/2017
+ *      Author: jcrespo
+ */
+
+#include "unity.h"
+#include "bema.h"
+#include <stdint.h>
+
+
+void setUp(){
+
+}
+
+void tearDown(){
+	;
+}
+
+// Test 1
+void testLedDriverCreateDebeIniciarConLedsApagados(){
+
+}
