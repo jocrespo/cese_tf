647e764d6443697d1276a4ec6e325f1cd50accac
Jorge Crespo <jcrespo@tecnoaccion.com.ar>
2017-11-04 13:07:02 -0300

    Se añade test usb_comm

---
+++ test/test_usb_comm.c
@@ -0,0 +1,40 @@
+/*
+ * test_usb_comm.c
+ *
+ *  Created on: 4/9/2017
+ *      Author: jcrespo
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include "unity.h"
+#include "usb_comm.h"
+#include "error.h"
+
+//#include "mock_libusb.h"
+
+void setUp(){
+
+
+}
+
+void tearDown(){
+
+}
+
+void test_usb_comm_send_devuelve_el_error_correcto(){
+	int32_t ret;
+	uint16_t size;
+	unsigned char data[size];
+
+	// caso ok
+	libusb_bulk_transfer_IgnoreAndReturn(0);
+	ret=usb_comm_send(data,size);
+	TEST_ASSERT_EQUAL_INT32 (ret, size);
+
+	// caso ok
+	libusb_bulk_transfer_IgnoreAndReturn(1);
+	ret=usb_comm_send(data,size);
+	TEST_ASSERT_EQUAL_INT32 (ret, 1);
+}
