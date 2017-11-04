d642401b2a3a8c0c45b3fa94cad5e6dd9b5fe065
Jorge Crespo <jcrespo@tecnoaccion.com.ar>
2017-11-04 14:37:28 -0300

    Tests usb_comm

--- test/test_usb_comm.c
+++ test/test_usb_comm.c
@@ -22,7 +22,10 @@ void setUp(){
 void tearDown(){
 
 }
-/*
+
+/**
+ * Se comprueba que la funcion usb_comm_send devuelve el valor de retorno esperado
+ */
 void test_usb_comm_send_devuelve_el_error_correcto(){
 	int32_t ret;
 	uint16_t size;
@@ -33,9 +36,35 @@ void test_usb_comm_send_devuelve_el_error_correcto(){
 	ret=usb_comm_send(data,size);
 	TEST_ASSERT_EQUAL_INT32 (ret, size);
 
-	// caso ok
+	// casos nok
 	libusb_bulk_transfer_IgnoreAndReturn(1);
 	ret=usb_comm_send(data,size);
 	TEST_ASSERT_EQUAL_INT32 (ret, 1);
+
+	libusb_bulk_transfer_IgnoreAndReturn(-3);
+	ret=usb_comm_send(data,size);
+	TEST_ASSERT_EQUAL_INT32 (ret, -3);
 }
-*/
+
+/**
+ * Se comprueba que la funcion usb_comm_receive devuelve el valor de retorno esperado
+ */
+void test_usb_comm_receive_devuelve_el_error_correcto(){
+	int32_t ret;
+	uint16_t size;
+	unsigned char data[size];
+
+	// caso ok
+	libusb_bulk_transfer_IgnoreAndReturn(0);
+	ret=usb_comm_receive(data,size);
+	TEST_ASSERT_EQUAL_INT32 (ret, size);
+
+	// casos nok
+	libusb_bulk_transfer_IgnoreAndReturn(1);
+	ret=usb_comm_receive(data,size);
+	TEST_ASSERT_EQUAL_INT32 (ret, 1);
+
+	libusb_bulk_transfer_IgnoreAndReturn(-3);
+	ret=usb_comm_receive(data,size);
+	TEST_ASSERT_EQUAL_INT32 (ret, -3);
+}
